03f31307f636eb3e4218b8653ae6e6d58ea7f38e
deepakdhurve6588-debug <deepakdhurve6588@gmail.com>
2025-11-24 00:07:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.markdown("""
 .stApp::before {
     content: "";
     position: fixed;
-    top:0; left:0; width:100%; height:100%;
+    top:0; left:0; width:90%; height:100%;
     background: rgba(0,0,0,0.2);  /* overlay for text readability */
     pointer-events:none;
     z-index:0;
