7f3b982bd7ab8997c402fc1271f430ca840bf09e
deepakdhurve6588-debug <deepakdhurve6588@gmail.com>
2025-11-28 22:17:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ st.markdown("""
 
 /* Card */
 .card {
-    background: rgba(255,255,255,255);
+    background: rgba(230,255,255,0.5);
     padding: 20px;
     border-radius: 14px;
     border: 1px solid rgba(255,255,255,0.6);
